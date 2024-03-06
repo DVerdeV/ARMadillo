bae8ce57685fcd48c8ceeeafa4f1b5db20d029f3
JavierStark <javiertorralbocortes@gmail.com>
2024-03-06 10:30:29 +0100

    Init URperfect

---
+++ URperfect/URperfect.s
@@ -0,0 +1,27 @@
+.data
+res: .word 0
+numel: .word 4
+vec: .word 9,7,0,4
+.text
+main: ldr r5, =numel
+ldr r5, [r5]
+ldr r4, =vec
+....
+bl URperfect
+....
+ldr r2, =res
+str r8,[r2]
+ bx lr
+@ variable to store the result
+@ number of elements in vector
+@ call URperfect
+URperfect:
+
+loop:
+
+ bx lr
+@ declares function URperfect
+@
+@ the while probably
+@
+@ jump to address in link register lr (r14)
