81aef23eaa30c1bbce87d1a95af6aec37cb37426
JavierStark <javiertorralbocortes@gmail.com>
2024-03-07 15:35:23 +0100

    URperfect and main finished

--- URperfect/URperfect.s
+++ URperfect/URperfect.s
@@ -14,40 +14,49 @@ main:
 	ldr r4, =vec
 	mov r8, #0 @ sumPerfect
 	mov r9, #0 @ nrPerfect
-	mov r10, #0 @ maxperfect
+	mov r7, #0 @ maxperfect
 while:
 	cmp r5, #0
 	beq endwhile
+	ldr r0, [r4]
+	push {lr}
 	bl URperfect
+	pop {lr}
 	sub r5, r5, #1
 	cmp r0, #0
-	beq endcycle
-perfect:
+	beq currentNotPerfect
+currentPerfect:
+	ldr r0, [r4], #4
 	add r8, r8, r0
 	add r9, r9, #1
-	cmp r10, r0
+	cmp r7, r0
 	blt max
-	b endcycle
+	b while
 max:
-	mov r10, r0
-endcycle:
-	b endwhile
+	mov r7, r0
+	b while
+currentNotPerfect:
+	add r4, r4, #4
+	b while
 endwhile:
 	ldr r2, =res
 	str r8, [r2]
 	str r9, [r2, #4]
-	str r10, [r2, #8]
+	str r7, [r2, #8]
 	bx lr
 
 URperfect:
-	push {r4-r6, lr}
+	push {r4-r6}
 	cmp r0, #0
 	blt notPerfect
 	mov r1, #0
 findSqrt:
 	add r1, r1, #1
+	push {r2, lr}
 	bl Multiply
-	cmp r1, r0
+	mov r4, r2
+	pop {r2, lr}
+	cmp r4, r0
 	blt findSqrt
 	beq isPerfect
 	mov r0, #0
@@ -57,19 +66,21 @@ notPerfect:
 isPerfect:
 	mov r0, #1
 return:
-	pop {r4-r6, lr}
+	pop {r4-r6}
 	bx lr
 
-
+;
 Multiply:
-	push {r6, lr}
+	push {r6, r7}
 	mov r6, r1
+	mov r7, #0
 whilemultiplication:
-	cmp r0, #0
+	cmp r6, #0
 	beq endwhilemultiplication
-	add r0, r6, r6
+	add r7, r7, r1
 	sub r6, r6, #1
 	b whilemultiplication
 endwhilemultiplication:
-	pop {r6, lr}
+	mov r2, r7
+	pop {r6, r7}
 	bx lr
